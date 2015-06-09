02e4b83692a71564f5646fa7425f3197b24fc888
Mediesenteret <Mediesenteret@Mediesenterets-iMac-2.local>
2015-06-09 12:48:55 +0200

    Initial Commit

---
+++ FotoNHHS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FotoNHHS
+//
+//  Created by Mediesenteret on 09/06/15.
+//  Copyright © 2015 Max Ozerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
